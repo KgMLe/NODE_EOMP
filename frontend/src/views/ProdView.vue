475e682e0578f444b3dcf0556880c36772cd5f84
KgMLe <kgodisholebopa@gmail.com>
2023-08-21 16:35:04 +0200

    products page

--- frontend/src/views/ProdView.vue
+++ frontend/src/views/ProdView.vue
@@ -1,6 +1,43 @@
 <template>
   <div>
-<h1>PRODUCTS PAGE</h1>
+    <div class="bg-gray-100">
+  <div class="container-xl px-4 py-5">
+    <div class="mx-auto max-w-2xl py-4">
+      <h2 class="h2 text-gray-900">Collections</h2>
+
+      <div class="row row-cols-1 row-cols-md-3 g-4 mt-4">
+        <div class="col">
+          <div class="position-relative h-80 overflow-hidden rounded-lg bg-white aspect-ratio" >
+            <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg" alt="category" class="img-fluid h-100 w-100 object-cover">
+          </div>
+          <h3 class="mt-3 h6">
+            <a href="#" class="text-decoration-none text-gray-500">Cardio</a>
+          </h3>
+          <p class="text-sm font-weight-bold text-gray-900">Cardio machines to get the heart pumping. Suitable every space </p>
+        </div>
+        <div class="col">
+          <div class="position-relative h-80 overflow-hidden rounded-lg bg-white aspect-ratio">
+            <img src="" alt="category">
+          </div>
+          <h3 class="mt-3 h6">
+            <a href="#" class="text-decoration-none text-gray-500">Self-Improvement</a>
+          </h3>
+          <p class="text-sm font-weight-bold text-gray-900">Journals and note-taking</p>
+        </div>
+        <div class="col">
+          <div class="position-relative h-80 overflow-hidden rounded-lg bg-white aspect-ratio">
+            <img src="https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg" alt="category" class="img-fluid h-100 w-100 object-cover">
+          </div>
+          <h3 class="mt-3 h6">
+            <a href="#" class="text-decoration-none text-gray-500">Travel</a>
+          </h3>
+          <p class="text-sm font-weight-bold text-gray-900">Daily commute essentials</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
   </div>
 </template>
 
