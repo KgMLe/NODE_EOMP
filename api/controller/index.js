2733ae4a490533a71b68964d2caee9163d4d9ae1
S1bu <Sibusisodyan@gmail.com>
2023-08-21 10:06:11 +0200

    started with api and folder structure

---
+++ api/controller/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const routes = express.Router();
+
+//--------------------------------------
+//----------------------------------------------------
+//-----------routing to USERS-----------------------
+
+//---------import all model objects------
+const { users } = require("../model");
+
+//fetch all user
+routes.get("/users", (req, res) => {
+    users.fetchUsers(req, res);
+  });
+  //fetch 1 user
+  routes.get("/user/:id", (req, res) => {
+    users.fetchUser(req, res);
+  });
+  //register user
+  routes.post("/register", bodyParser.json(), (req, res) => {
+    users.register(req, res);
+  });
+  //update user
+  routes.patch("/users/:id", bodyParser.json(), (req, res) => {
+    users.updateUser(req, res);
+  });
+  //delete user
+  routes.delete("/user/:id", (req, res) => {
+    users.deleteUser(req, res);
+  });
+  //login a user 
+  routes.post("/login", bodyParser.json(), (req, res) => {
+      users.login(req, res);
+    });
